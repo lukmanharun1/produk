ac22697d06f1aa27c9b0ad441d612de1b784cff0
Lukman Harun <nandes88.ni@gmail.com>
2022-08-25 18:02:46 +0700

    membuat helper jwt

---
+++ helpers/jwt.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const { LOGIN_TOKEN_SECRET } = process.env;
+
+const createTokenLogin = (payload) =>
+  new Promise((resolve, reject) =>
+    jwt.sign(
+      payload,
+      LOGIN_TOKEN_SECRET,
+      { expiresIn: "7 days" },
+      (err, data) => {
+        if (err)
+          reject({
+            message: error,
+            statusCode: 422,
+          });
+        resolve(data);
+      }
+    )
+  );
+
+const verifyTokenLogin = (token) =>
+  new Promise((resolve, reject) =>
+    jwt.verify(token, LOGIN_TOKEN_SECRET, (err, decoded) => {
+      if (err)
+        reject({
+          message: error,
+          statusCode: 422,
+        });
+      resolve(decoded);
+    })
+  );
+
+module.exports = {
+  createTokenLogin,
+  verifyTokenLogin,
+};
