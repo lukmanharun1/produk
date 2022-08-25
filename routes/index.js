394e09336916ac15ddb4b14dda1553b6b38fcd51
Lukman Harun <nandes88.ni@gmail.com>
2022-08-25 17:28:25 +0700

    autoload routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,17 @@
+const fs = require("fs/promises");
+
 const express = require("express");
 const router = express.Router();
+
+async function readRoutes() {
+  const files = await fs.readdir("./routes");
+  for (const file of files) {
+    const [uri] = file.split(".");
+    if (uri !== "index") {
+      router.use(`/${uri}`, require(`./${file}`));
+    }
+  }
+}
+readRoutes();
 
 module.exports = router;
