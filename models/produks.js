6f7f9aa06f97f1d34abb73b0b9feb4cf58252cca
Lukman Harun <nandes88.ni@gmail.com>
2022-08-25 20:51:35 +0700

    membuat model produks

---
+++ models/produks.js
@@ -0,0 +1,46 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Produks extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Produks.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+      },
+      kode_produk: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+      },
+      nama_produk: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+      },
+      qty: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      timestamps: true,
+      modelName: "Produks",
+      tableName: "produks",
+      freezeTableName: true,
+      underscored: true,
+      paranoid: true,
+    }
+  );
+  return Produks;
+};
