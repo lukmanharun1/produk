ea70d3fd0e8c3ba1367229df9763e49aa8d3b0a2
Lukman Harun <nandes88.ni@gmail.com>
2022-08-25 20:50:09 +0700

    membuat routes produks

---
+++ routes/produks.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const controller = require("../controllers/produks");
+const validate = require("../middlewares/validate");
+const validation = require("../validations/produks");
+const authentication = require("../middlewares/authentication");
+const uploadImage = require("../middlewares/multer");
+
+router.post(
+  "/",
+  authentication,
+  uploadImage,
+  validation.create(),
+  validate,
+  controller.create
+);
+
+module.exports = router;
