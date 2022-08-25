45cc13d0f096d07726964943e568d7b6d9bfa41e
Lukman Harun <nandes88.ni@gmail.com>
2022-08-25 20:51:21 +0700

    membuat migration produks

---
+++ migrations/20220825123326-create-produks-table.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const fields = require("../constants/fields");
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      "produks",
+      fields(Sequelize, {
+        kode_produk: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+        },
+        nama_produk: {
+          type: Sequelize.STRING(128),
+          allowNull: false,
+        },
+        qty: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+      })
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("produks");
+  },
+};
