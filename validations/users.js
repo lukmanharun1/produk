fe0e7db8eeefb539c5cd9f15950e4267c86dd2fa
Lukman Harun <nandes88.ni@gmail.com>
2022-08-25 17:29:13 +0700

    membuat validation users

---
+++ validations/users.js
@@ -0,0 +1,12 @@
+const { body } = require("express-validator");
+
+const register = () => [
+  body("email").isEmail(),
+  body("first_name").isString().isLength({ min: 1, max: 128 }),
+  body("last_name").isString().isLength({ min: 1, max: 128 }),
+  body("password").isStrongPassword(),
+];
+
+module.exports = {
+  register,
+};
