7d26089f1a2405b208fb2bf43ff1f20b18d492db
Lukman Harun <nandes88.ni@gmail.com>
2022-08-25 22:35:04 +0700

    menambahkan controller (getById, update) produks

--- controllers/produks.js
+++ controllers/produks.js
@@ -4,10 +4,10 @@ const hapusFile = require("../helpers/hapus_file");
 const paginate = require("../helpers/paginate");
 
 const { HOST, PORT } = process.env;
+const pathUpload = `${HOST}:${PORT}/produks/`;
 const getAll = async (req, res) => {
   try {
     const { page, per_page } = req.query;
-    const pathUpload = `${HOST}:${PORT}/produks/`;
     const { count, rows } = await Produks.findAndCountAll({
       offset: (page - 1) * page,
       limit: per_page,
@@ -16,6 +16,7 @@ const getAll = async (req, res) => {
         "id",
         "kode_produk",
         "nama_produk",
+        "qty",
         [
           Sequelize.fn("CONCAT", pathUpload, Sequelize.col("image_produk")),
           "src_image_produk",
@@ -32,6 +33,42 @@ const getAll = async (req, res) => {
     return response(res, {
       status: "success",
       data,
+    });
+  } catch (error) {
+    return response(
+      res,
+      {
+        status: "error",
+        message: error.message,
+      },
+      error.statusCode || 500
+    );
+  }
+};
+const getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const findProduksById = await Produks.findByPk(id, {
+      attributes: [
+        "id",
+        "kode_produk",
+        "nama_produk",
+        "qty",
+        [
+          Sequelize.fn("CONCAT", pathUpload, Sequelize.col("image_produk")),
+          "src_image_produk",
+        ],
+      ],
+    });
+    if (!findProduksById) {
+      throw {
+        message: "Data produk tidak ada!",
+        statusCode: 404,
+      };
+    }
+    return response(res, {
+      status: "success",
+      data: findProduksById,
     });
   } catch (error) {
     return response(
@@ -91,8 +128,75 @@ const create = async (req, res) => {
     );
   }
 };
+const update = async (req, res) => {
+  try {
+    const { kode_produk, nama_produk, qty } = req.body;
+    const { id } = req.params;
+    // find produk by id
+    const findProduksById = await Produks.findByPk(id);
+    if (!findProduksById) {
+      throw {
+        message: "Data produk tidak ada!",
+        statusCode: 404,
+      };
+    }
+    let isDeleteImage = false;
+    if (req.file) isDeleteImage = true;
+
+    if (nama_produk) {
+      // cek agar tidak duplikat
+      const findNamaProduks = await Produks.findOne({ where: { nama_produk } });
+      if (findNamaProduks) {
+        throw {
+          message: "nama produk sudah ada!",
+          statusCode: 400,
+        };
+      }
+    }
+
+    const [isUpdateProduk] = await Produks.update(
+      {
+        kode_produk,
+        nama_produk,
+        qty,
+        image_produk: req.file?.filename,
+      },
+      {
+        where: {
+          id,
+        },
+      }
+    );
+    if (!isUpdateProduk) {
+      throw {
+        message: "Data produk gagal diupdate!",
+      };
+    }
+    if (isDeleteImage) {
+      // hapus file yang lama
+      console.log(findProduksById.image_produk);
+      await hapusFile(`public/produks/${findProduksById.image_produk}`);
+    }
+    return response(res, {
+      status: "success",
+      message: "Data produk berhasil diupdate!",
+    });
+  } catch (error) {
+    if (req.file) await hapusFile(req.file.path);
+    return response(
+      res,
+      {
+        status: "error",
+        message: error.message,
+      },
+      error.statusCode || 500
+    );
+  }
+};
 
 module.exports = {
   getAll,
+  getById,
   create,
+  update,
 };
