d4dc6873f22c817443ff1b654a69f6d557ae536d
Lukman Harun <nandes88.ni@gmail.com>
2022-08-25 21:41:00 +0700

    menambahkan validation (getAll) produks

--- validations/produks.js
+++ validations/produks.js
@@ -1,5 +1,9 @@
-const { body } = require("express-validator");
+const { query, body } = require("express-validator");
 
+const getAll = () => [
+  query("page").isInt({ gt: 0 }).toInt(),
+  query("per_page").isInt({ gt: 0 }).toInt(),
+];
 const create = () => [
   body("kode_produk").isString().isLength({ max: 12 }),
   body("nama_produk").isString().isLength({ min: 3, max: 128 }),
@@ -7,5 +11,6 @@ const create = () => [
 ];
 
 module.exports = {
+  getAll,
   create,
 };
