5fe2c485676e303a2b21406fabf29ae9aea1d4d7
Lukman Harun <nandes88.ni@gmail.com>
2022-08-25 16:10:27 +0700

    test authtentication mysql

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.json")[env];
+const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
 let sequelize;
@@ -40,17 +40,7 @@ Object.keys(db).forEach((modelName) => {
   }
 });
 
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-})();
-
 db.sequelize = sequelize;
-async;
 db.Sequelize = Sequelize;
 
 module.exports = db;
